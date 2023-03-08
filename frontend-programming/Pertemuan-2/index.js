b7f428a9d75d13051a610532e69fc7fd014651d8
Salmarida <92412404+Salmarida@users.noreply.github.com>
2023-03-08 05:53:11 -1200

    Add files via upload

---
+++ frontend-programming/Pertemuan-2/index.js
@@ -0,0 +1,35 @@
+/**
+* Menjalankan Test: Array of Objec Users
+*
+*/ 
+
+const user = ["Aufa", "Isfhani Ghiyath", "Nurul", "Sabiq"];
+   
+console.log(user[1], user[1]);
+
+console.log("#Get All Users");
+
+console.log("# Add new user: Sabiq");
+const newUser = {
+    name: "Sabiq",
+    age: 20,
+    major: "informatics",
+};
+console.log("# Add new user: Nurul");
+constnewUser = {
+    name: "Nurul",
+    age: 21,
+    major: "Informatics",
+};
+
+
+console.log("# Edit User: Isfa");
+const editedUser = {
+    name: "Isfhani Ghiyath",
+    age: 23,
+    major: "English",
+};
+
+console.log("# Delete User: Nurul");
+destroy(2);
+
